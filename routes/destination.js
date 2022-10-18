500c4cb4afe13f178ecc5f595737968469e4da5e
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-10-18 11:41:06 -1000

    Set up routing

--- routes/destination.js
+++ routes/destination.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/destination/create", () => res.send("Route for creating"));
+router.post("/destination/create", (req, res) =>
+  res.send("Route for creating")
+);
+
+router.put("/destination/update", (req, res) => res.send("Route for updating"));
+
+router.delete("/destination/delete/:id", (req, res) =>
+  res.send("Route for deleting")
+);
+
+router.get("/destinations/", (req, res) => res.send("Route for getting all"));
+
+router.get("/destination/:id", (req, res) =>
+  res.send("Route for getting by id")
+);
 
 module.exports = router;
